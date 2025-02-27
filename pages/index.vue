1120afab261e2eb56f833a0dfa7d6295cc2d7d0d
laixander <laixander@gmail.com>
2025-02-27 11:43:34 +0800

    test commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
-    <UContainer>
-        <ColorMode />
-        <h1 class="text-gray-800 dark:text-gray-100">Hello World</h1>
-    </UContainer>
+    <div class="flex flex-col h-screen">
+        <header class="p-4 flex items-center justify-between">
+            <h1 class="text-gray-800 dark:text-gray-100">Hello World</h1>
+            <ColorMode />
+        </header>
+        <main class="flex-1 flex items-center">
+            <UContainer>
+                <p class="text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, ad totam? Voluptas, reiciendis minus placeat aliquam nesciunt commodi adipisci id?</p>
+            </UContainer>
+        </main>
+    </div>
 </template>
